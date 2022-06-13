a73ec667008c83305bbfdc208437683d191e4d1b
Chris <cjmugs@gmail.com>
2022-06-13 18:43:29 -0500

    updates

--- Basic Data/ranges.py
+++ Basic Data/ranges.py
@@ -1,4 +1,10 @@
 # Ranges 
 Student_Grades = [9.1, 10.9, 16.9]
-for x in range(0,10000):
+for x in range(0,100):
     print(Student_Grades)
+    
+    
+    # or
+    
+sg = list(range(0,1100000, 3))
+print(sg)
